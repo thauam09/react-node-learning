c1600cad38746c9ba1b78f7f45d2f117da230329
Thauã Martins <tm17042762@shi-g.com>
2020-05-30 19:30:47 -0300

    Updates-project-my-expenses

---
+++ react-course/tratamento-de-erros.js
@@ -0,0 +1,25 @@
+class CustomError extends Error{
+    constructor({message, data}){
+        super(message);
+        this.data = data;
+    }
+}
+
+try{
+    //console.log(name);
+    const name = 'Thauã Martins';
+
+    const myError = new CustomError({message: 'Custom message on custom error',
+        data: {
+            type: 'Server error'
+        }
+    });
+
+    throw myError;
+} catch(err){
+    console.log(err);
+    console.log(err.data);
+}finally{
+    console.log('Keep Going...');    
+}
+
